2a490b7b7fb0bfc22a683b79f9eed06bc82bbc46
unknown <przemyslaw.bialczak@pjwstk.edu.pl>
2019-11-24 18:24:06 +0100

    dodalem wyswietlanie sie usera

--- main.py
+++ main.py
@@ -141,12 +141,12 @@ def prepare_recommendation(json_data, users):
 
     ####################################################
 
-    print('\nMovies recommended for U:\n')
+    print('\nMovies recommended for ' + user +':\n')
 
     for i, j in sorted_movies_rated_by_most_common_critics[-10:]:
         print(i, j)
 
-    print('\nMovies NOT recommended for U:\n')
+    print('\nMovies NOT recommended for ' + user +':\n')
 
     for i, j in sorted_movies_rated_by_most_common_critics[:10]:
         print(i, j)
